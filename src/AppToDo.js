ef88013311a50272ca399fc959942f093b2f58d5
Targonkp Targonkp <targon_k_p@mail.ru>
2022-07-08 00:36:45 +0300

    Fixed in code: added correct ID

--- src/AppToDo.js
+++ src/AppToDo.js
@@ -11,17 +11,13 @@ function AppToDo() {
     //состояние списка задач
     const [toDoList, setToDoList] = useState(tasks)
     //получаю id последнего элемента в списке, чтобы избежать дублирования в дальнейшем
-    let lastElement;
-    if (toDoList.length === 0){
-        lastElement = 1
-    }
-    else {
-        lastElement = toDoList.length-1
-    }
+    let lastElement
+    lastElement = toDoList.length-1
 
     //создаю функцию, которая будет добавлять новую задачу в list - для этого передаю ее в виде пропса в ToDoForm
     function addNewTask(inputValue, currentDate) {
-        let newList = [...toDoList, {id: toDoList[lastElement].id + 1, description: inputValue, complete: false, date: currentDate}]
+        //здесь в id вложил условие, если длина массива окажется равной одному, то id при добавлении нового элемента будет равно 1
+        let newList = [...toDoList, {id: toDoList.length === 0 ? 1 : toDoList[lastElement].id + 1, description: inputValue, complete: false, date: currentDate}]
         setToDoList(newList)
     }
 
